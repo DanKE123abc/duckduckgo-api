bb135271a51a58821bc5c5ee9643f2b25a075ce7
DanKe <1586017466@QQ.com>
2025-10-06 16:55:55 +0800

    测试2

--- app.py
+++ app.py
@@ -58,10 +58,14 @@ async def suggest():
 @app.route('/search', methods=['GET', 'POST'])
 async def search():
     keywords, max_results = run()
-    results = []
     
-    from ddgs import DDGS
-    results = DDGS().text(keywords, max_results)
+    results = []
+    with DDGS() as ddgs:
+        # 使用DuckDuckGo搜索关键词
+        ddgs_gen = ddgs.text(keywords)
+        # 从搜索结果中获取最大结果数
+        for r in islice(ddgs_gen, max_results):
+            results.append(r)
 
     # 返回一个json响应，包含搜索结果
     return {'results': results}
